905112a7e3f6530fd4316c440fa44a4babbdaae5
satosh <smartdev.house108@gmail.com>
2022-12-20 16:46:59 +0200

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Router from 'routes';
+
+function App() {
+  return (
+    <Router />
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return <h1>
+    {'This is home screen'}
+  </h1>
+}
+
+export default Home
