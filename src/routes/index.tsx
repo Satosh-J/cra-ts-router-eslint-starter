b70f0c4f7ab303f5d293cfbfd2d11a7b0e37f640
satosh <smartdev.house108@gmail.com>
2022-12-20 16:55:16 +0200

    remove all warnings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,17 @@
-import { FC } from "react";
+import { FC } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route
-} from "react-router-dom";
-import Home from "pages/Home";
+} from 'react-router-dom';
+import Home from 'pages/Home';
 
 
 const Router: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
